b92eab4f24ef5a3fc2b3dea16f587ac4e48c6413
Torry Brelsford <Torry.Brelsford@caci.com>
2018-01-29 16:11:34 -0700

    context menu

---
+++ open-sphere-plugins/my-places/src/main/java/io/opensphere/myplaces/controllers/RoiContextMenuProvider.java
@@ -0,0 +1,60 @@
+package io.opensphere.myplaces.controllers;
+
+import java.awt.Component;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import io.opensphere.core.Toolbox;
+import io.opensphere.core.control.action.ContextMenuProvider;
+import io.opensphere.core.util.collections.New;
+import io.opensphere.mantle.data.CategoryContextKey;
+import io.opensphere.mantle.data.DataGroupInfo;
+import io.opensphere.mantle.data.DataGroupInfo.DataGroupContextKey;
+import io.opensphere.mantle.data.DataTypeInfo;
+import io.opensphere.myplaces.constants.Constants;
+import io.opensphere.myplaces.models.MyPlacesModel;
+
+public class RoiContextMenuProvider implements ContextMenuProvider<DataGroupInfo.DataGroupContextKey>
+{
+
+    /**
+     * Provides the context menu for group modifications.
+     *
+     */
+    /**
+     * The toolbox.
+     */
+    private final Toolbox myToolbox;
+
+    /**
+     * The places model.
+     */
+    private final MyPlacesModel myModel;
+
+    /**
+     * Constructs a new points context menu provider.
+     *
+     * @param toolbox The toolbox.
+     * @param model The my places model.
+     */
+    public RoiContextMenuProvider(Toolbox toolbox, MyPlacesModel model)
+    {
+        myToolbox = toolbox;
+        myModel = model;
+    }
+
+    @Override
+    public Collection<? extends Component> getMenuItems(String contextId, DataGroupContextKey key)
+    {
+        List<Component> menuItems;
+
+        return menuItems;
+    }
+
+    @Override
+    public int getPriority()
+    {
+        return 6;
+    }
+}
