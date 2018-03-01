f4a68134d0c7ae688f85132e4e0fc272511d26f3
jkae <siegel.raphael@gmail.com>
2018-03-01 11:39:23 -0700

    Spreadsheet Utility Class (#22)
    * Initial.
    
    * Spreadsheet utility update.
    
    * VORTEX-5633 : Replace listener with SpreadsheetUtil and move to models package.
    
    * SpreadsheetUtil rename
    
    * Final / constructor.

---
+++ open-sphere-base/analysis/src/main/java/io/opensphere/analysis/table/model/SpreadsheetUtilities.java
@@ -0,0 +1,49 @@
+package io.opensphere.analysis.table.model;
+
+import javax.swing.JTable;
+
+/**
+ * Spreadsheet-related utilities for table models.
+ */
+public final class SpreadsheetUtilities
+{
+    /**
+     * Constructor.
+     */
+    private SpreadsheetUtilities()
+    {
+    }
+
+    /**
+     * Sums the values of the selected table cells.
+     *
+     * @param table the table
+     * @return summation
+     */
+    public static double getSelectedSum(JTable table)
+    {
+        double sum = 0.0;
+
+        int[] rows = table.getSelectedRows();
+        int[] cols = table.getSelectedColumns();
+        for (int r : rows)
+        {
+            for (int c : cols)
+            {
+                Object value = table.getValueAt(r, c);
+
+                try
+                {
+                    sum += Double.valueOf(value.toString()).doubleValue();
+                }
+                catch (NumberFormatException | NullPointerException e)
+                {
+                    // Ignore non-numeric values.
+                    continue;
+                }
+            }
+        }
+
+        return sum;
+    }
+}
