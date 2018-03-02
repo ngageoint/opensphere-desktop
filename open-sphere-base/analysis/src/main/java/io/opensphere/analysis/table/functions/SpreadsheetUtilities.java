1236af8dabf1226a9907d9112a9038a676ff47e1
Jacob Siegel <jacob.siegel@caci.com>
2018-03-02 10:40:03 -0700

    Refactoring.

---
+++ open-sphere-base/analysis/src/main/java/io/opensphere/analysis/table/functions/SpreadsheetUtilities.java
@@ -0,0 +1,27 @@
+package io.opensphere.analysis.table.functions;
+
+/**
+ * Spreadsheet-related utilities for table models.
+ */
+public final class SpreadsheetUtilities
+{
+    /** Function values. */
+    private static final SpreadsheetFunction[] FUNCTION_VALUES = new SpreadsheetFunction[] { new SpreadsheetFunction.Sum() };
+
+    /**
+     * Constructor.
+     */
+    private SpreadsheetUtilities()
+    {
+    }
+
+    /**
+     * Retrieves all standard Function values.
+     *
+     * @return array of defined values
+     */
+    public static SpreadsheetFunction[] getFunctions()
+    {
+        return FUNCTION_VALUES;
+    }
+}
