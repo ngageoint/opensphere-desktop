54e905a46aa52ce64d2f677edd0f543c012efea7
Jacob Siegel <jacob.siegel@caci.com>
2018-03-21 14:14:41 -0600

    Probably faster to reduce list of image managers to a set, assuming similar geometries use the same one.

--- open-sphere-base/core/src/main/java/io/opensphere/core/pipeline/processor/PointSpriteProcessor.java
+++ open-sphere-base/core/src/main/java/io/opensphere/core/pipeline/processor/PointSpriteProcessor.java
@@ -353,7 +353,10 @@ public class PointSpriteProcessor extends TextureProcessor<PointSpriteGeometry>
                     List<PointSpriteGeometry> projectionSensitiveGeoms = getGeometries().parallelStream()
                             .filter(g -> g.isProjectionSensitive()).collect(Collectors.toCollection(New::list));
 
-                    projectionSensitiveGeoms.forEach(geom -> getImageManagersToGeoms().remove(geom.getImageManager()));
+                    projectionSensitiveGeoms.parallelStream().map(geom -> geom.getImageManager())
+                            .collect(Collectors.toCollection(New::set))
+                            .forEach(manager -> getImageManagersToGeoms().remove(manager));
+
                     resetDueToImageUpdate(projectionSensitiveGeoms, State.UNPROCESSED);
                 }
             }
