7cf375b369b547008bda6cba2476aa1d7cb557a2
Jacob Siegel <jacob.siegel@caci.com>
2018-03-28 09:51:43 -0600

    Completed GovIcon comments

--- open-sphere-base/core/src/main/java/io/opensphere/core/util/GovIcon.java
+++ open-sphere-base/core/src/main/java/io/opensphere/core/util/GovIcon.java
@@ -3,279 +3,415 @@ package io.opensphere.core.util;
 /** An enumeration over the set of available govicons.io icons. */
 public enum GovIcon implements FontIconEnum
 {
-    /** */
+    /** Enumerable representation of the 540 logo icon. */
     LOGO_540("\ue900"),
-    /** */
+
+    /** Enumerable representation of the 540 federal API icon. */
     FEDAPI_540("\ue933"),
-    /**  */
+
+    /** Enumerable representation of the 18F logo icon. */
     LOGO_18F("\ue971"),
-    /** */
+
+    /** Enumerable representation of an airplane icon. */
     AIRPLANE("\ue919"),
-    /** */
+
+    /** Enumerable representation of an ambulance icon. */
     AMBULANCE("\ue980"),
-    /** */
+
+    /** Enumerable representation of an ammunition icon. */
     AMMO("\ue910"),
-    /** */
+
+    /** Enumerable representation of a balance icon. */
     BALANCE("\ue912"),
-    /** */
+
+    /** Enumerable representation of a barcode icon. */
     BARCODE("\ue95e"),
-    /** */
+
+    /** Enumerable representation of a bar chart icon. */
     BAR_CHART("\ue908"),
-    /** */
+
+    /** Enumerable representation of the biohazard icon. */
     BIOHAZARD("\ue964"),
-    /** */
+
+    /** Enumerable representation of a book icon. */
     BOOK("\ue945"),
-    /** */
+
+    /** Enumerable representation of a briefcase icon. */
     BRIEFCASE("\ue929"),
-    /** */
+
+    /** Enumerable representation of a building icon. */
     BUILDING("\ue949"),
-    /** */
+
+    /** Enumerable representation of a bus (front) icon. */
     BUS_FRONT("\ue914"),
-    /** */
+
+    /** Enumerable representation of a CAC icon. */
     CAC("\ue944"),
-    /** */
+
+    /** Enumerable representation of a CAC icon. */
     CAC_O("\ue943"),
-    /** */
+
+    /** Enumerable representation of the Capitol Building icon. */
     CAPITOL("\ue94b"),
-    /** */
+
+    /** Enumerable representation of the caution icon. */
     CAUTION("\ue924"),
-    /** */
+
+    /** Enumerable representation of a certification icon. */
     CERT("\ue925"),
-    /** */
+
+    /** Enumerable representation of a checkmark icon. */
     CHECK("\ue922"),
-    /** */
+
+    /** Enumerable representation of a checkmark (inside of a square) icon. */
     CHECK_SQUARE_O("\ue923"),
-    /** */
+
+    /** Enumerable representation of a clock icon. */
     CLOCK_O("\ue903"),
-    /** */
+
+    /** Enumerable representation of a cloud icon. */
     CLOUD("\ue906"),
-    /** */
+
+    /** Enumerable representation of a cloud icon. */
     CLOUD_O("\ue905"),
-    /** */
+
+    /** Enumerable representation of a code icon. */
     CODE("\ue904"),
-    /** */
+
+    /** Enumerable representation of a cog icon. */
     COG("\ue931"),
-    /** */
+
+    /** Enumerable representation of a cog (multiple) icon. */
     COGS("\ue932"),
-    /** */
+
+    /** Enumerable representation of a comment icon. */
     COMMENT("\ue90f"),
-    /** */
+
+    /** Enumerable representation of a comment (multiple) icon. */
     COMMENTS("\ue93a"),
-    /** */
+
+    /** Enumerable representation of a construction icon. */
     CONSTRUCTION("\ue960"),
-    /** */
+
+    /** Enumerable representation of a database icon. */
     DATABASE("\ue90a"),
-    /** */
+
+    /** Enumerable representation of the DC flag icon. */
     DC_FLAG("\ue939"),
-    /** */
+
+    /** Enumerable representation of a map icon (of DC). */
     DC_MAP("\ue600"),
-    /** */
+
+    /** Enumerable representation of a DNA icon. */
     DNA("\ue966"),
-    /** */
+
+    /** Enumerable representation of a dogtag icon. */
     DOGTAGS("\ue96e"),
-    /** */
+
+    /** Enumerable representation of a donkey icon. */
     DONKEY("\ue92d"),
-    /** */
+
+    /** Enumerable representation of a drone icon. */
     DRONE("\ue953"),
-    /** */
+
+    /** Enumerable representation of an elephant icon. */
     ELEPHANT("\ue92e"),
-    /** */
+
+    /** Enumerable representation of a factory icon. */
     FACTORY("\ue972"),
-    /** */
+
+    /** Enumerable representation of a file icon. */
     FILE("\ue93f"),
-    /** */
+
+    /** Enumerable representation of a file (contract) icon. */
     FILE_CONTRACT_O("\ue937"),
-    /** */
+
+    /** Enumerable representation of a file (Excel) icon. */
     FILE_EXCEL("\ue973"),
-    /** */
+
+    /** Enumerable representation of a file (Excel) icon. */
     FILE_EXCEL_O("\ue93e"),
-    /** */
+
+    /** Enumerable representation of a file icon. */
     FILE_O("\ue941"),
-    /** */
+
+    /** Enumerable representation of a file (TXT) icon. */
     FILE_TEXT("\ue940"),
-    /** */
+
+    /** Enumerable representation of a file (TXT) icon. */
     FILE_TEXT_O("\ue938"),
-    /** */
+
+    /** Enumerable representation of a file (Word) icon. */
     FILE_WORD("\ue974"),
-    /** */
+
+    /** Enumerable representation of a file (Word) icon. */
     FILE_WORD_O("\ue93d"),
-    /** */
+
+    /** Enumerable representation of a fingerprint icon. */
     FINGERPRINT("\ue961"),
-    /** */
+
+    /** Enumerable representation of a folder icon. */
     FOLDER("\ue927"),
-    /** */
+
+    /** Enumerable representation of a folder (misc) icon. */
     FOLDER_MISC("\ue928"),
-    /** */
+
+    /** Enumerable representation of a gavel icon. */
     GAVEL("\ue913"),
-    /** */
+
+    /** Enumerable representation of a gear icon. */
     GEAR("\ue931"),
-    /** */
+
+    /** Enumerable representation of a gear (multiple) icon. */
     GEARS("\ue932"),
-    /** */
+
+    /** Enumerable representation of a globe icon. */
     GLOBE("\ue947"),
-    /** */
+
+    /** Enumerable representation of a gun icon. */
     GUN("\ue915"),
-    /** */
+
+    /** Enumerable representation of a handshake icon. */
     HANDSHAKE("\ue934"),
-    /** */
+
+    /** Enumerable representation of a hardhat icon. */
     HARD_HAT("\ue975"),
-    /** */
+
+    /** Enumerable representation of a heartbeat icon. */
     HEARTBEAT("\ue90b"),
-    /** */
+
+    /** Enumerable representation of a helicopter icon. */
     HELICOPTER("\ue918"),
-    /** */
+
+    /** Enumerable representation of an ID card icon. */
     ID_CARD("\ue944"),
-    /** */
+
+    /** Enumerable representation of an ID card icon. */
     ID_CARD_O("\ue943"),
-    /** */
+
+    /** Enumerable representation of the Jefferson Memorial icon. */
     JEFFERSON_MEMORIAL("\ue976"),
-    /** */
+
+    /** Enumerable representation of a jet icon. */
     JET("\ue91a"),
-    /** */
+
+    /** Enumerable representation of a key icon. */
     KEY("\ue926"),
-    /** */
+
+    /** Enumerable representation of a leaf icon. */
     LEAF("\ue951"),
-    /** */
+
+    /** Enumerable representation of the Liberty Bell icon. */
     LIBERTY_BELL("\ue95d"),
-    /** */
+
+    /** Enumerable representation of a lightbulb icon. */
     LIGHTBULB("\ue901"),
-    /** */
+
+    /** Enumerable representation of the Lincoln Memorial icon. */
     LINCOLN_MEMORIAL("\ue977"),
-    /** */
+
+    /** Enumerable representation of a line chart icon. */
     LINE_CHART("\ue909"),
-    /** */
+
+    /** Enumerable representation of a lock icon. */
     LOCK("\ue930"),
-    /** */
+
+    /** Enumerable representation of a medal (circle) icon. */
     MEDAL_CIRCLE("\ue96f"),
-    /** */
+
+    /** Enumerable representation of a medal (star) icon. */
     MEDAL_STAR("\ue970"),
-    /** */
+
+    /** Enumerable representation of a medkit icon. */
     MEDKIT("\ue90c"),
-    /** */
+
+    /** Enumerable representation of a train/metro (front) icon. */
     METRO_FRONT("\ue978"),
-    /** */
+
+    /** Enumerable representation of a microscope icon. */
     MICROSCOPE("\ue965"),
-    /** */
+
+    /** Enumerable representation of a missile icon. */
     MISSILE("\ue954"),
-    /** */
+
+    /** Enumerable representation of a mobile icon. */
     MOBILE("\ue91d"),
-    /** */
+
+    /** Enumerable representation of a mobile phone icon. */
     MOBILE_PHONE("\ue91d"),
-    /** */
+
+    /** Enumerable representation of a money icon. */
     MONEY("\ue91f"),
-    /** */
+
+    /** Enumerable representation of a nuclear plant icon. */
     NUCLEAR_PLANT("\ue962"),
-    /** */
+
+    /** Enumerable representation of a pallet icon. */
     PALLET("\ue979"),
-    /** */
+
+    /** Enumerable representation of the Pentagon icon. */
     PENTAGON("\ue94c"),
-    /** */
+
+    /** Enumerable representation of a pie chart icon. */
     PIE_CHART("\ue907"),
-    /** */
+
+    /** Enumerable representation of the poison icon. */
     POISON("\ue97a"),
-    /** */
+
+    /** Enumerable representation of a poison bottle icon. */
     POISON_BOTTLE("\ue97b"),
-    /** */
+
+    /** Enumerable representation of a presenter icon. */
     PRESENTER("\ue93c"),
-    /** */
+
+    /** Enumerable representation of a radar icon. */
     RADAR("\ue96b"),
-    /** */
+
+    /** Enumerable representation of the radioactive icon. */
     RADIOACTIVE("\ue963"),
-    /** */
+
+    /** Enumerable representation of a reticle (crosshair) icon. */
     RETICLE_CROSSHAIR("\ue96d"),
-    /** */
+
+    /** Enumerable representation of a reticle (dot) icon. */
     RETICLE_DOT("\ue96c"),
-    /** */
+
+    /** Enumerable representation of the recycle icon. */
     RECYCLE("\ue952"),
-    /** */
+
+    /** Enumerable representation of a ribbon icon. */
     RIBBON("\ue925"),
-    /** */
+
+    /** Enumerable representation of a road icon. */
     ROAD("\ue968"),
-    /** */
+
+    /** Enumerable representation of a road barricade icon. */
     ROAD_BARRICADE("\ue969"),
-    /** */
+
+    /** Enumerable representation of a road sign icon. */
     ROAD_SIGN("\ue967"),
-    /** */
+
+    /** Enumerable representation of a ruler icon. */
     RULER("\ue95f"),
-    /** */
+
+    /** Enumerable representation of a satellite icon. */
     SATELLITE("\ue955"),
-    /** */
+
+    /** Enumerable representation of a script icon. */
     SCRIPT("\ue902"),
-    /** */
+
+    /** Enumerable representation of a search icon. */
     SEARCH("\ue942"),
-    /** */
+
+    /** Enumerable representation of a security camera icon. */
     SECURITY_CAMERA("\ue94f"),
-    /** */
+
+    /** Enumerable representation of a shield icon. */
     SHIELD("\ue957"),
-    /** */
+
+    /** Enumerable representation of a shield icon. */
     SHIELD_O("\ue956"),
-    /** */
+
+    /** Enumerable representation of a ship icon. */
     SHIP("\ue911"),
-    /** */
+
+    /** Enumerable representation of the Statue of Liberty icon. */
     STATUE_OF_LIBERTY("\ue95c"),
-    /** */
+
+    /** Enumerable representation of a stethoscope icon. */
     STETHOSCOPE("\ue90d"),
-    /** */
+
+    /** Enumerable representation of a submarine icon. */
     SUBMARINE("\ue96a"),
-    /** */
+
+    /** Enumerable representation of a table icon. */
     TABLE("\ue92a"),
-    /** */
+
+    /** Enumerable representation of a table (multiple) icon. */
     TABLES("\ue92b"),
-    /** */
+
+    /** Enumerable representation of a tablet icon. */
     TABLET("\ue91e"),
-    /** */
+
+    /** Enumerable representation of a tank icon. */
     TANK("\ue91b"),
-    /** */
+
+    /** Enumerable representation of an icon. */
     TIMES("\ue921"),
-    /** */
+
+    /** Enumerable representation of a textile icon. */
     TEXTILE("\ue950"),
-    /** */
+
+    /** Enumerable representation of a tools icon. */
     TOOLS("\ue90e"),
-    /** */
+
+    /** Enumerable representation of a train (front) icon. */
     TRAIN_FRONT("\ue97c"),
-    /** */
+
+    /** Enumerable representation of a truck icon. */
     TRUCK("\ue917"),
-    /** */
+
+    /** Enumerable representation of a truck (front) icon. */
     TRUCK_FRONT("\ue916"),
-    /** */
+
+    /** Enumerable representation of an unlock icon. */
     UNLOCK("\ue92f"),
-    /** */
+
+    /** Enumerable representation of the United States flag (straight) icon. */
     US_FLAG_STRAIGHT("\ue95b"),
-    /** */
+
+    /** Enumerable representation of the United States flag (wavy) icon. */
     US_FLAG_WAVY("\ue948"),
-    /** */
+
+    /** Enumerable representation of a map icon. */
     US_MAP("\ue946"),
-    /** */
+
+    /** Enumerable representation of a tophat icon. */
     US_TOPHAT("\ue92c"),
-    /** */
+
+    /** Enumerable representation of a shield icon. */
     US_SHIELD("\ue958"),
-    /** */
+
+    /** Enumerable representation of the United States dollar. */
     USD("\ue920"),
-    /** */
+
+    /** Enumerable representation of a user icon. */
     USER("\ue93b"),
-    /** */
+
+    /** Enumerable representation of a user (graduate) icon. */
     USER_GRADUATE("\ue94e"),
-    /** */
+
+    /** Enumerable representation of a user (military) icon. */
     USER_MILITARY("\ue935"),
-    /** */
+
+    /** Enumerable representation of a user (politician) icon. */
     USER_POLITICIAN("\ue959"),
-    /** */
+
+    /** Enumerable representation of a user (student) icon. */
     USER_STUDENT("\ue94e"),
-    /** */
+
+    /** Enumerable representation of a user (suit) icon. */
     USER_SUIT("\ue936"),
-    /** */
+
+    /** Enumerable representation of a user (multiple) icon. */
     USERS("\ue95a"),
-    /** */
+
+    /** Enumerable representation of a vote icon. */
     VOTE("\ue94d"),
-    /** */
+
+    /** Enumerable representation of a warning icon. */
     WARNING("\ue924"),
-    /** */
+
+    /** Enumerable representation of the Washington Monument icon. */
     WASHINGTON_MONUMENT("\ue94a"),
-    /** */
+
+    /** Enumerable representation of a wheelbarrow icon. */
     WHEEL_BARROW("\ue97d"),
-    /** */
+
+    /** Enumerable representation of the White House icon. */
     WHITE_HOUSE("\ue97e"),
-    /** */
+
+    /** Enumerable representation of a world icon. */
     WORLD("\ue947");
 
     /**
