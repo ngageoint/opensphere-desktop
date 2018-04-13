dee4ca85d5a1f051e2bd9af1c154ace13c35601a
brelsftm <torry.brelsford@caci.com>
2018-04-13 14:56:01 -0600

    fixed point context menu bug

--- open-sphere-plugins/my-places/src/main/java/io/opensphere/myplaces/controllers/RoiContextMenuProvider.java
+++ open-sphere-plugins/my-places/src/main/java/io/opensphere/myplaces/controllers/RoiContextMenuProvider.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.swing.JMenuItem;
 
+import de.micromata.opengis.kml.v_2_2_0.Point;
 import io.opensphere.core.Toolbox;
 import io.opensphere.core.control.action.ContextMenuProvider;
 import io.opensphere.core.geometry.PolygonGeometry;
@@ -33,7 +34,8 @@ public class RoiContextMenuProvider implements ContextMenuProvider<DataGroupCont
     {
         myToolbox = toolbox;
     }
-
+    
+    @Override
     public List<JMenuItem> getMenuItems(String contextId, DataGroupContextKey key)
     {
         List<JMenuItem> menuItems = Collections.emptyList();
@@ -41,11 +43,13 @@ public class RoiContextMenuProvider implements ContextMenuProvider<DataGroupCont
         {
 
             MyPlacesDataTypeInfo type = (MyPlacesDataTypeInfo)key.getDataType();
+            Object placemarkGeom = type.getKmlPlacemark().getGeometry();
 
-            if (type.getKmlPlacemark().getGeometry() instanceof Track)
+            if (placemarkGeom instanceof Track || placemarkGeom instanceof Point)
             {
                 return menuItems;
             }
+
             PolygonGeometry geom = RegionUtils.createGeometry(type.getKmlPlacemark());
             menuItems = MantleToolboxUtils.getMantleToolbox(myToolbox).getSelectionHandler().getGeometryMenuItems(geom);
         }
