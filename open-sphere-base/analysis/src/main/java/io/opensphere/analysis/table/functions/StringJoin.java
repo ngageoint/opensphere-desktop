3d14dbe6d8a10f946296a7404e8dd0b92735e5da
Jacob Siegel <jacob.siegel@caci.com>
2018-05-02 11:37:24 -0600

    Update StringJoin

--- open-sphere-base/analysis/src/main/java/io/opensphere/analysis/table/functions/StringJoin.java
+++ open-sphere-base/analysis/src/main/java/io/opensphere/analysis/table/functions/StringJoin.java
@@ -1,6 +1,7 @@
 package io.opensphere.analysis.table.functions;
 
-import org.apache.commons.lang.StringUtils;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 /** Joins all values in an array as a single String. */
 public class StringJoin extends ColumnFunction
@@ -8,7 +9,25 @@ public class StringJoin extends ColumnFunction
     /** Constructs a StringJoin function. */
     protected StringJoin()
     {
-        super("String Join", 0, StringUtils::join);
+        super("String Join", 0, StringJoin::performJoin);
     }
 
+    /**
+     * Joins array of Objects to a single String, utilizing
+     * {@link Objects#toString(Object)} as the String parser.
+     * <p>
+     * Resulting Strings will be in the format "0,1,...".
+     *
+     * @param objects the objects to operate on
+     * @return the string
+     */
+    static String performJoin(Object... objects)
+    {
+        StringJoiner sj = new StringJoiner(",", "", "");
+        for (Object o : objects)
+        {
+            sj.add(Objects.toString(o));
+        }
+        return sj.toString();
+    }
 }
