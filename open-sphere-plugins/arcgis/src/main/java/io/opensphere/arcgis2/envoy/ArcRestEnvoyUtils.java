3485dc12916218e7c08a3ca98a97ba761d767b26
brelsftm <torry.brelsford@caci.com>
2018-05-04 09:44:35 -0600

    replaces any wildcards with %

--- open-sphere-plugins/arcgis/src/main/java/io/opensphere/arcgis2/envoy/ArcRestEnvoyUtils.java
+++ open-sphere-plugins/arcgis/src/main/java/io/opensphere/arcgis2/envoy/ArcRestEnvoyUtils.java
@@ -90,14 +90,16 @@ public final class ArcRestEnvoyUtils
         // Add user-defined filter
         if (filter != null)
         {
+
             try
             {
-                parameters.put("where", URLEncoder.encode(filter.getSqlLikeString(), "UTF-8"));
+                parameters.put("where", URLEncoder.encode(filter.getSqlLikeString().replace("*", "%"), "UTF-8"));
             }
             catch (UnsupportedEncodingException e)
             {
                 throw new MalformedURLException("Error encoding filter: " + e);
             }
+            
         }
 
         // Request only IDs
@@ -108,6 +110,7 @@ public final class ArcRestEnvoyUtils
 
         builder.setQueryParameters(parameters);
 
+        System.out.println(builder.toURL());
         return builder.toURL();
     }
 
