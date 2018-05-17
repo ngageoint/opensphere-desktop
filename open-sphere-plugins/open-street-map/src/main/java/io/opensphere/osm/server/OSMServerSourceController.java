5b66eda5e19859154cca7b1241ae2a5b497d62bf
Daniel Lueth <daniel.lueth@caci.com>
2018-05-17 15:34:49 -0600

    VORTEX-5764 First cut

--- open-sphere-plugins/open-street-map/src/main/java/io/opensphere/osm/server/OSMServerSourceController.java
+++ open-sphere-plugins/open-street-map/src/main/java/io/opensphere/osm/server/OSMServerSourceController.java
@@ -1,9 +1,19 @@
 package io.opensphere.osm.server;
 
+import org.apache.log4j.Logger;
+
+import io.opensphere.core.Notify;
+import io.opensphere.core.Notify.Method;
+import io.opensphere.core.Toolbox;
 import io.opensphere.core.cache.SimpleSessionOnlyCacheDeposit;
 import io.opensphere.core.data.util.DataModelCategory;
+import io.opensphere.core.image.Image;
+import io.opensphere.core.model.GeographicBoundingBox;
+import io.opensphere.core.model.LatLonAlt;
+import io.opensphere.core.model.ZYXImageKey;
 import io.opensphere.core.server.ServerProviderRegistry;
 import io.opensphere.core.util.collections.New;
+import io.opensphere.core.util.taskactivity.TaskActivity;
 import io.opensphere.mantle.datasources.IDataSource;
 import io.opensphere.mantle.datasources.impl.UrlDataSource;
 import io.opensphere.osm.util.OSMUtil;
@@ -15,11 +25,25 @@ import io.opensphere.server.toolbox.ServerSourceController;
 import io.opensphere.xyztile.model.Projection;
 import io.opensphere.xyztile.model.XYZServerInfo;
 import io.opensphere.xyztile.model.XYZTileLayerInfo;
+import io.opensphere.xyztile.transformer.XYZImageProvider;
 import io.opensphere.xyztile.util.XYZTileUtils;
 
 /** Open Street Map's {@link ServerSourceController}. */
 public class OSMServerSourceController extends UrlServerSourceController
 {
+    /** Logger reference. */
+    private static final Logger LOGGER = Logger.getLogger(OSMServerSourceController.class);
+
+    /** The core toolbox. */
+    private Toolbox myToolbox;
+
+    @Override
+    public void open(Toolbox toolbox, Class<?> prefsTopic)
+    {
+        myToolbox = toolbox;
+        super.open(toolbox, prefsTopic);
+    }
+
     @Override
     public IDataSource createNewSource(String typeName)
     {
@@ -47,21 +71,43 @@ public class OSMServerSourceController extends UrlServerSourceController
     @Override
     protected boolean handleActivateSource(IDataSource source)
     {
-        DataModelCategory category = XYZTileUtils.newLayersCategory(((UrlDataSource)source).getURL(), OSMUtil.PROVIDER);
-        XYZServerInfo serverInfo = new XYZServerInfo(source.getName(), ((UrlDataSource)source).getURL());
-        XYZTileLayerInfo layerInfo = null;
-        if (serverInfo.getServerUrl().contains("osm_tiles_pc"))
-        {
-            layerInfo = new XYZTileLayerInfo(OSMUtil.PROVIDER, source.getName(), Projection.EPSG_4326, 2, false, 2, serverInfo);
-        }
-        else
-        {
-            layerInfo = new XYZTileLayerInfo(OSMUtil.PROVIDER, source.getName(), Projection.EPSG_3857, 1, false, 5, serverInfo);
-        }
+        boolean pingSuccess = false;
 
-        getToolbox().getDataRegistry()
-                .addModels(new SimpleSessionOnlyCacheDeposit<>(category, XYZTileUtils.LAYERS_DESCRIPTOR, New.list(layerInfo)));
-        return true;
+        try (TaskActivity activity = TaskActivity.createActive(source.getName() + " is loading..."))
+        {
+            myToolbox.getUIRegistry().getMenuBarRegistry().addTaskActivity(activity);
+
+            DataModelCategory category = XYZTileUtils.newLayersCategory(((UrlDataSource)source).getURL(), OSMUtil.PROVIDER);
+            XYZServerInfo serverInfo = new XYZServerInfo(source.getName(), ((UrlDataSource)source).getURL());
+            XYZTileLayerInfo layerInfo = null;
+            if (serverInfo.getServerUrl().contains("osm_tiles_pc"))
+            {
+                layerInfo = new XYZTileLayerInfo(OSMUtil.PROVIDER, source.getName(), Projection.EPSG_4326, 2, false, 2,
+                        serverInfo);
+            }
+            else
+            {
+                layerInfo = new XYZTileLayerInfo(OSMUtil.PROVIDER, source.getName(), Projection.EPSG_3857, 1, false, 5,
+                        serverInfo);
+            }
+
+            XYZImageProvider imageProvider = new XYZImageProvider(myToolbox.getDataRegistry(), layerInfo);
+            Image pingImage = null;
+            pingImage = imageProvider
+                    .getImage(new ZYXImageKey(2, 1, 5, new GeographicBoundingBox(LatLonAlt.createFromDegrees(0., 45.),
+                            LatLonAlt.createFromDegrees(45., 90.))));
+            pingSuccess = (pingImage != null);
+            if (pingSuccess)
+            {
+                getToolbox().getDataRegistry().addModels(
+                        new SimpleSessionOnlyCacheDeposit<>(category, XYZTileUtils.LAYERS_DESCRIPTOR, New.list(layerInfo)));
+            }
+            else
+            {
+                Notify.error("Failed to query OSM server: Could not establish connection during intial ping", Method.TOAST);
+            }
+        }
+        return pingSuccess;
     }
 
     @Override
