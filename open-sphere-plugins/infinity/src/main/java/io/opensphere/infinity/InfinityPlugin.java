4131f619fef5b71f2d1a605074ded38e699efd69
Adam Roddy <aroddy@mail.com>
2018-05-29 14:40:37 -0600

    Create infinity plugin

---
+++ open-sphere-plugins/infinity/src/main/java/io/opensphere/infinity/InfinityPlugin.java
@@ -0,0 +1,19 @@
+package io.opensphere.infinity;
+
+import io.opensphere.core.PluginLoaderData;
+import io.opensphere.core.Toolbox;
+import io.opensphere.core.api.adapter.PluginAdapter;
+
+/** Infinity (Elasticsearch) plugin. */
+public class InfinityPlugin extends PluginAdapter
+{
+    @Override
+    public void initialize(PluginLoaderData plugindata, Toolbox toolbox)
+    {
+    }
+
+    @Override
+    public void close()
+    {
+    }
+}
