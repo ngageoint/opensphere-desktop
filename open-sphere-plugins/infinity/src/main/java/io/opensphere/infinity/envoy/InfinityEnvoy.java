b90cb7cd5e6e0efca97e8fccff74c31c15a7226f
Adam Roddy <aroddy@mail.com>
2018-06-01 16:50:31 -0600

    Infinity envoy stuff

--- open-sphere-plugins/infinity/src/main/java/io/opensphere/infinity/envoy/InfinityEnvoy.java
+++ open-sphere-plugins/infinity/src/main/java/io/opensphere/infinity/envoy/InfinityEnvoy.java
@@ -11,19 +11,28 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import com.vividsolutions.jts.geom.Geometry;
+
 import io.opensphere.core.Toolbox;
 import io.opensphere.core.api.adapter.SimpleEnvoy;
 import io.opensphere.core.cache.CacheDeposit;
 import io.opensphere.core.cache.CacheException;
 import io.opensphere.core.cache.DefaultCacheDeposit;
+import io.opensphere.core.cache.accessor.GeometryAccessor;
+import io.opensphere.core.cache.accessor.TimeSpanAccessor;
 import io.opensphere.core.cache.accessor.UnserializableAccessor;
+import io.opensphere.core.cache.matcher.PropertyMatcher;
+import io.opensphere.core.cache.util.IntervalPropertyValueSet;
 import io.opensphere.core.cache.util.PropertyDescriptor;
 import io.opensphere.core.data.CacheDepositReceiver;
 import io.opensphere.core.data.DataRegistry;
 import io.opensphere.core.data.QueryException;
 import io.opensphere.core.data.util.DataModelCategory;
+import io.opensphere.core.data.util.OrderSpecifier;
+import io.opensphere.core.data.util.Satisfaction;
 import io.opensphere.core.data.util.SimpleQuery;
 import io.opensphere.core.model.time.TimeInstant;
+import io.opensphere.core.model.time.TimeSpan;
 import io.opensphere.core.server.ContentType;
 import io.opensphere.core.server.HttpServer;
 import io.opensphere.core.server.ResponseValues;
@@ -59,6 +68,7 @@ public class InfinityEnvoy extends SimpleEnvoy<SearchResponse>
      */
     public static SearchResponse query(DataRegistry dataRegistry, String layerUrl) throws QueryException
     {
+        // TODO query similar to RecommendedLayersEnvoy
         DataModelCategory category = new DataModelCategory(null, FAMILY, layerUrl);
         SimpleQuery<SearchResponse> query = new SimpleQuery<>(category, PROPERTY_DESCRIPTOR);
         List<SearchResponse> results = performQuery(dataRegistry, query);
@@ -82,33 +92,69 @@ public class InfinityEnvoy extends SimpleEnvoy<SearchResponse>
     }
 
     @Override
-    protected void query(URL url, DataModelCategory category, CacheDepositReceiver queryReceiver)
-        throws IOException, CacheException, QueryException
+    public void query(DataModelCategory category, Collection<? extends Satisfaction> satisfactions,
+            List<? extends PropertyMatcher<?>> parameters, List<? extends OrderSpecifier> orderSpecifiers, int limit,
+            Collection<? extends PropertyDescriptor<?>> propertyDescriptors, CacheDepositReceiver queryReceiver)
+        throws InterruptedException, QueryException
     {
-        if (url != null)
+        try
         {
-            SearchRequest request = new SearchRequest();
-            request.setAggs(new Aggs("field1.keyword", 10000, 1000000000000000000L));
-
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            JsonUtils.createMapper().writeValue(out, request);
-
-            InputStream postData = new ByteArrayInputStream(out.toByteArray());
-            ResponseValues response = new ResponseValues();
-            ServerProvider<HttpServer> provider = getServerProviderRegistry().getProvider(HttpServer.class);
-            try (CancellableInputStream inputStream = HttpUtilities.sendPost(url, postData, response, ContentType.JSON, provider))
+            URL url = getUrl(category);
+            String binField = "field1";
+            if (url != null)
             {
-                Collection<SearchResponse> items = parseDepositItems(inputStream);
-                if (!items.isEmpty())
+                for (Satisfaction sat : satisfactions)
                 {
-                    CacheDeposit<SearchResponse> deposit = createDeposit(category, items);
-                    queryReceiver.receive(deposit);
+                    IntervalPropertyValueSet valueSet = sat.getIntervalPropertyValueSet();
+                    Collection<? extends Geometry> geometries = valueSet.getValues(GeometryAccessor.PROPERTY_DESCRIPTOR);
+                    Collection<? extends TimeSpan> timeSpans = valueSet.getValues(TimeSpanAccessor.PROPERTY_DESCRIPTOR);
+                    for (Geometry geometry : geometries)
+                    {
+                        for (TimeSpan timeSpan : timeSpans)
+                        {
+                            query(category, queryReceiver, url, geometry, timeSpan, binField);
+                        }
+                    }
                 }
             }
+            else
+            {
+                LOGGER.info(getClass().getName() + " envoy's getUrl method returned a null value, skipping query.");
+            }
         }
-        else
+        catch (IOException | CacheException e)
         {
-            LOGGER.info(getClass().getName() + " envoy's getUrl method returned a null value, skipping query.");
+            throw new QueryException(e);
+        }
+    }
+
+    private void query(DataModelCategory category, CacheDepositReceiver queryReceiver, URL url, Geometry geometry,
+            TimeSpan timeSpan, String binField)
+        throws IOException, CacheException
+    {
+        System.out.println(timeSpan + " " + geometry);
+
+        SearchRequest request = new SearchRequest();
+        //  TODO populate with geom/time data
+        if (binField != null)
+        {
+            request.setAggs(new Aggs(binField + ".keyword", 10000, 1000000000000000000L));
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        JsonUtils.createMapper().writeValue(out, request);
+
+        InputStream postData = new ByteArrayInputStream(out.toByteArray());
+        ResponseValues response = new ResponseValues();
+        ServerProvider<HttpServer> provider = getServerProviderRegistry().getProvider(HttpServer.class);
+        try (CancellableInputStream inputStream = HttpUtilities.sendPost(url, postData, response, ContentType.JSON, provider))
+        {
+            Collection<SearchResponse> items = parseDepositItems(inputStream);
+            if (!items.isEmpty())
+            {
+                CacheDeposit<SearchResponse> deposit = createDeposit(category, items);
+                queryReceiver.receive(deposit);
+            }
         }
     }
 
