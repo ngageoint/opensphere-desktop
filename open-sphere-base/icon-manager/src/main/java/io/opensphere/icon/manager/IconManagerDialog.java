51bbdb07e3ea2dc270fb8be1a6c3bc50bdc345f3
kcrombie <kelly.crombie@caci.com>
2018-06-27 14:15:08 -0600

    test

--- open-sphere-base/icon-manager/src/main/java/io/opensphere/icon/manager/IconManagerDialog.java
+++ open-sphere-base/icon-manager/src/main/java/io/opensphere/icon/manager/IconManagerDialog.java
@@ -1,5 +1,11 @@
 package io.opensphere.icon.manager;
 
+import io.opensphere.mantle.icon.IconRecord;
+import io.opensphere.mantle.icon.IconRegistry;
+import io.opensphere.mantle.icon.impl.gui.IconBuilderDialog;
+import io.opensphere.mantle.icon.impl.gui.IconChooserDialog;
+import io.opensphere.mantle.icon.impl.gui.IconChooserPanel;
+import io.opensphere.mantle.util.MantleToolboxUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
@@ -15,46 +21,59 @@ import javafx.stage.Stage;
 
 public class IconManagerDialog
 {
-
+    /** The AnchorPane myTopMenuBar; Top window of GUI */
     @FXML
     private AnchorPane myTopMenuBar;
 
+    /** The ButtonBar mySizeMenu; Top Right Buttons of GUI */
     @FXML
     private ButtonBar mySizeMenu;
 
+    /** The Text mySizeLabel; The centered text label */
     @FXML
     private Text mySizeLabel;
 
+    /** The Button myShrinkButton; centered minus sign */
     @FXML
     private Button myShrinkButton;
 
+    /** The Button myEnlargeButton; centered plus sign */
     @FXML
     private Button myEnlargeButton;
 
+    /** The Hbox myViewToggle; holds toggle buttons List/Grid */
     @FXML
     private HBox myViewToggle;
 
+    /* The Text myViewLabel; labels view buttons */
     @FXML
     private Text myViewLabel;
 
+    /* The RadioButton myListView; switch for list view */
     @FXML
     private RadioButton myListView;
 
-    @FXML
-    private ToggleGroup ViewStyle;
-
+    /* The RadioButton myGridView; switch for grid view */
     @FXML
     private RadioButton myGridView;
 
+    /* The ToggleGroup ViewStyle; contains the two buttons */
+    @FXML
+    private ToggleGroup ViewStyle;
+
+    /* The ButtonBar mySearchBar; contains search functions*/
     @FXML
     private ButtonBar mySearchBar;
 
+    /* The ButtonBar mySearchLabel; contains search label*/
     @FXML
     private Text mySearchLabel;
 
+    /* The AnchorPane myTreeView; contains Tree features*/
     @FXML
     private AnchorPane myTreeView;
-
+    
+    /* The AnchorPane myTreeView; contains the file tree*/
     @FXML
     private TreeView<?> myTreeList;
 
@@ -78,7 +97,12 @@ public class IconManagerDialog
 
     @FXML
     private Text myNotifyText;
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /**
+     
+     */
+    private boolean myChoice;
 
     @FXML
     void ADD(MouseEvent event)
@@ -86,6 +110,7 @@ public class IconManagerDialog
 
     }
 
+    /** The Busy label. */
     @FXML
     void CUSTOMIZE(MouseEvent event)
     {
@@ -98,19 +123,20 @@ public class IconManagerDialog
         // get a handle to the stage
         Stage stage = (Stage)myCloseButton.getScene().getWindow();
         stage.close();
-
     }
 
     @FXML
-    void ENLARGE(MouseEvent event)
+    void shrink(MouseEvent event)
     {
-
+        myChoice = false;
+        setIconSize(myChoice);
     }
 
     @FXML
-    void GENERATE(MouseEvent event)
+    void enlarge(MouseEvent event)
     {
-
+        myChoice = true;
+        setIconSize(myChoice);
     }
 
     @FXML
@@ -122,9 +148,10 @@ public class IconManagerDialog
     @FXML
     void IMPORT(MouseEvent event)
     {
+        // IconChooserPanel.loadFromFile(IconRecord.USER_ADDED_COLLECTION, null)
         // Button.setOnAction(loadFromFile(IconRecord.USER_ADDED_COLLECTION,
         // null));
-    };
+    }
 
     @FXML
     void LISTV(MouseEvent event)
@@ -139,10 +166,29 @@ public class IconManagerDialog
     }
 
     @FXML
-    void handleAddIconButtonAction(MouseEvent event)
+    void GENERATE(MouseEvent event)
     {
-        //myNotifyText.setText("Icon Added to Favorites");
-        //loadFromFile(IconRecord.USER_ADDED_COLLECTION, null);
+
     }
 
+    @FXML
+    void handleAddIconButtonAction(MouseEvent event)
+    {
+        // myNotifyText.setText("Icon Added to Favorites");
+        // loadFromFile(IconRecord.USER_ADDED_COLLECTION, null);
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void setIconSize(boolean myChoice)
+    {
+        if (myChoice)
+        {
+            System.out.println("you enlarged Icons");
+        }
+        else
+        {
+            System.out.println("you shrank icons");
+        }
+
+    }
 }
