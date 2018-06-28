b409b30eff4aa8b40f3565ffac1a90ea48f4b44f
kcrombie <kelly.crombie@caci.com>
2018-06-28 14:32:33 -0600

    GUI Converted into pure JavaFX

--- open-sphere-base/icon-manager/src/main/java/io/opensphere/icon/manager/IconManagerFrame.java
+++ open-sphere-base/icon-manager/src/main/java/io/opensphere/icon/manager/IconManagerFrame.java
@@ -1,42 +1,126 @@
 package io.opensphere.icon.manager;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class IconManagerFrame extends Application
+import io.opensphere.icon.manager.IconManagerBuilder;
+
+/**
+ * This is the main control for the Icon Manager GUI. All button actions and
+ * styling are contained in class.
+ */
+public class IconManagerFrame extends IconManagerBuilder
 {
-    @Override
-    public void start(Stage primaryStage)
-    {
-        try
-        {
-            AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("IconManagerFrameTest.fxml"));
-            Scene scene = new Scene(root, 720, 400);
-            scene.getStylesheets().add(getClass().getResource("IconManagerFrameStyle.css").toExternalForm());
-            primaryStage.setScene(scene);
-            Image myWindowIcon = new Image(new FileInputStream("src/main/resources/Images/caci.jpg"));
-            primaryStage.getIcons().add(myWindowIcon);
-            primaryStage.setTitle("Icon Manager");
-            primaryStage.setMinHeight(400);
-            primaryStage.setMinWidth(720);
-            primaryStage.show();
 
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+    /* private TreeItem<String> myTreeRoot;
+     * 
+     * private TreeItem<String> myFavorites;
+     * 
+     * private TreeItem<String> myUserAdded;
+     * 
+     * private TreeItem<String> myGoogleEarth;
+     * 
+     * private TreeItem<String> myMilStd;
+     * 
+     * private TreeView<String> myTreeListTemp;
+     * 
+     * TreeView<String> myTree; */
 
-    }
+    private Scene myScene;
 
     public static void main(String[] args)
     {
         launch(args);
+
+    }
+
+    public void start(Stage myIconManagerInterFace) throws FileNotFoundException
+    {
+
+        AnchorPane myMainAnchorPane = createIconManagerPane();
+
+        myScene = new Scene(myMainAnchorPane, 720, 400);
+        myScene.getStylesheets().add(getClass().getResource("iconmanager.css").toExternalForm());
+        myIconManagerInterFace.setScene(myScene);
+        myIconManagerInterFace.setMinHeight(400);
+        myIconManagerInterFace.setMinWidth(720);
+        Image myWindowIcon = new Image(new FileInputStream("src/main/resources/Images/caci.jpg"));
+        myIconManagerInterFace.getIcons().add(myWindowIcon);
+        myIconManagerInterFace.setTitle("Icon Manager");
+
+        myIconManagerInterFace.show();
+
+    }
+
+    public TreeItem<String> createBranch(String branchName, TreeItem<String> rootName)
+    {
+        TreeItem<String> localBranch = new TreeItem<>(branchName);
+        localBranch.setExpanded(true);
+
+        rootName.getChildren().add(localBranch);
+        return (localBranch);
+    }
+
+    @Override
+    public void shrink(MouseEvent mouseEvent)
+    {
+
+    }
+
+    @Override
+    public void enlarge(MouseEvent mouseEvent)
+    {
+
+    }
+
+    @Override
+    public void LISTV(MouseEvent mouseEvent)
+    {
+
+    }
+
+    @Override
+    public void GRIDV(MouseEvent mouseEvent)
+    {
+
+    }
+
+    @Override
+    public void ADD(MouseEvent mouseEvent)
+    {
+
+    }
+
+    @Override
+    public void handleAddIconButtonAction(MouseEvent mouseEvent)
+    {
+
+    }
+
+    @Override
+    public void CUSTOMIZE(MouseEvent mouseEvent)
+    {
+
+    }
+
+    @Override
+    public void GENERATE(MouseEvent mouseEvent)
+    {
+
+    }
+
+    @Override
+    public void Close(MouseEvent mouseEvent)
+    {
+        Stage myCurrentStage = (Stage)myCloseButton.getScene().getWindow();
+        myCurrentStage.close();
+
     }
 }
