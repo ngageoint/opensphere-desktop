7b7d7d4c7bcd2548e5dcaecc23bf49d54f6164a0
Gabby Barbieri <gabriella.barbieri@caci.com>
2018-07-09 15:13:02 -0600

    "assorted ch

--- open-sphere-base/mantle/src/main/java/io/opensphere/mantle/iconproject/view/MainPanel.java
+++ open-sphere-base/mantle/src/main/java/io/opensphere/mantle/iconproject/view/MainPanel.java
@@ -1,9 +1,21 @@
 package io.opensphere.mantle.iconproject.view;
 
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Window;
+import java.io.File;
+import java.net.MalformedURLException;
+
 import io.opensphere.core.Toolbox;
-import io.opensphere.mantle.icon.impl.gui.IconChooserPanel;
+import io.opensphere.core.util.image.ImageUtil;
+import io.opensphere.core.util.swing.EventQueueUtilities;
+import io.opensphere.core.util.swing.JTreeUtilities;
+import io.opensphere.mantle.icon.IconRecord;
+import io.opensphere.mantle.icon.IconRecordTreeNodeUserObject;
+import io.opensphere.mantle.icon.IconRegistry;
+import io.opensphere.mantle.icon.impl.DefaultIconProvider;
+import io.opensphere.mantle.icon.impl.gui.IconTreeBuilder;
+import io.opensphere.mantle.util.MantleToolboxUtils;
 
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.SplitPane;
@@ -13,8 +25,30 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+
 public class MainPanel extends SplitPane
 {
+    /** The Icon registry. */
+    private final IconRegistry myIconRegistry;
+
+    /** The optional selected icon URL. */
+    private String mySelectedUrl;
+
+    /** The Last selected tree node user object. */
+    private IconRecordTreeNodeUserObject myLastSelectedTreeNodeUserObject;
+    //these are in swing vvvvv
+    /** The Tree. */
+    private final JTree myTree;
+    /** The Root tree node. */
+    @SuppressWarnings("PMD.SingularField")
+    private TreeNode myRootTreeNode;
+    /** The Tree model. */
+    private DefaultTreeModel myTreeModel;
+    //^^^^^ need to convert swing to fx
 
     RowConstraints rowConstraints = new RowConstraints();
 
@@ -37,8 +71,15 @@ public class MainPanel extends SplitPane
     @SuppressWarnings("unused")
     public MainPanel(Toolbox tb)
     {
+        myTree = new JTree();
+        myTree.setRootVisible(false);
+        myTree.setExpandsSelectedPaths(true);
 
+
+
+        myIconRegistry = MantleToolboxUtils.getMantleToolbox(tb).getIconRegistry();
         Window owner = tb.getUIRegistry().getMainFrameProvider().get();
+
 
         myTreeView = new AnchorPane();
         myTreeList = new TreeView();
@@ -60,8 +101,8 @@ public class MainPanel extends SplitPane
 
         myAddIconButton.setOnAction(event ->
         {
-            IconChooserPanel chooseIcon = new IconChooserPanel(tb);
-            // loadFromFile(IconRecord.USER_ADDED_COLLECTION, null);
+            // IconChooserPanel chooseIcon = new IconChooserPanel(tb);
+            loadFromFile(IconRecord.USER_ADDED_COLLECTION, null, tb);
         });
 
         AnchorPane.setBottomAnchor(myCustIconButton, 26.0);
@@ -93,5 +134,141 @@ public class MainPanel extends SplitPane
         gridPane.getRowConstraints().addAll(rowConstraints);
 
         getItems().addAll(myTreeView, gridPane, myScrollBar);
+    }
+
+    /**
+     * Load from file.
+     *
+     * @param collectionName the collection name
+     * @param subCatName the sub cat name
+     */
+    public void loadFromFile(String collectionName, String subCatName, Toolbox myToolbox)
+    {
+        Component parent = myToolbox.getUIRegistry().getMainFrameProvider().get();
+        File result = ImageUtil.showImageFileChooser("Choose Icon File", parent, myToolbox.getPreferencesRegistry());
+
+        if (result != null)
+        {
+            try
+            {
+                myIconRegistry.addIcon(new DefaultIconProvider(result.toURI().toURL(), collectionName, subCatName, "User"), this);
+                refreshFromRegistry(collectionName);
+            }
+            catch (MalformedURLException e)
+            {
+                //JOptionPane.showMessageDialog(this, "Failed to load image: " + result.getAbsolutePath(), "Image Load Error",
+                        //JOptionPane.ERROR_MESSAGE);
+                System.out.println("Failed to load image");
+            }
+        }
+    }
+
+    /**
+     * Refresh from registry.
+     *
+     * @param collectionToShow the name of the collection to show, or null
+     */
+    public final void refreshFromRegistry(String collectionToShow)
+    {
+        EventQueueUtilities.runOnEDT(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                if (myTree != null)
+                {
+                    myLastSelectedTreeNodeUserObject = null;
+                    myRootTreeNode = new IconTreeBuilder(myIconRegistry).getIconRecordTree(null);
+                    if (myTreeModel == null)
+                    {
+                        myTreeModel = new DefaultTreeModel(myRootTreeNode);
+                    }
+                    else
+                    {
+                        myTreeModel.setRoot(myRootTreeNode);
+                    }
+                    myTree.setModel(myTreeModel);
+                    myTree.revalidate();
+                    JTreeUtilities.expandOrCollapseAll(myTree, true);
+
+                    TreeNode nodeToSelect = getNodeToSelect(collectionToShow);
+                    if (nodeToSelect != null)
+                    {
+                        TreeNode[] nodeArray = myTreeModel.getPathToRoot(nodeToSelect);
+                        TreePath path = new TreePath(nodeArray);
+                        myTree.getSelectionModel().setSelectionPath(path);
+                    }
+                }
+            }
+        });
+    }
+    /**
+     * Gets the node to select.
+     *
+     * @param collectionToShow the requested collection to show
+     * @return the node to select, or null
+     */
+    private TreeNode getNodeToSelect(String collectionToShow)
+    {
+        TreeNode nodeToSelect = null;
+
+        // If there is a requested collection to show, find its node
+        if (collectionToShow != null)
+        {
+            for (int i = 0; i < myRootTreeNode.getChildCount(); i++)
+            {
+                if (collectionToShow.equals(myRootTreeNode.getChildAt(i).toString()))
+                {
+                    nodeToSelect = myRootTreeNode.getChildAt(i);
+                    break;
+                }
+            }
+        }
+
+        if (nodeToSelect == null && myRootTreeNode.getChildCount() > 0)
+        {
+            // If there is a selected icon, find its node
+            //vvvvvvvvvv mySelectedUrl is set in IconChooserDialog and needs to be ported over somewhere
+            /*if (mySelectedUrl != null)
+            {
+                for (int i = 0; i < myRootTreeNode.getChildCount(); i++)
+                {
+                    TreeNode child = myRootTreeNode.getChildAt(i);
+                    if (child instanceof DefaultMutableTreeNode)
+                    {
+                        DefaultMutableTreeNode mtn = (DefaultMutableTreeNode)child;
+                        Object userObj = mtn.getUserObject();
+                        if (userObj instanceof IconRecordTreeNodeUserObject)
+                        {
+                            IconRecordTreeNodeUserObject irNode = (IconRecordTreeNodeUserObject)userObj;
+                            boolean hasIcon = irNode.getRecords(true).stream()
+                                    .anyMatch(r -> mySelectedUrl.equals(r.getImageURL().toString()));
+                            if (hasIcon)
+                            {
+                                nodeToSelect = mtn;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            // Default to the first one
+            else
+            {
+                nodeToSelect = myRootTreeNode.getChildAt(0);
+            }*/
+        }
+        return nodeToSelect;
+
+    }
+
+    /**
+     * Sets the selected icon URL.
+     *
+     * @param selectedUrl the icon URL
+     */
+    public void setSelectedUrl(String selectedUrl) //currently not in use?
+    {
+        mySelectedUrl = selectedUrl;
     }
 }
