f3ee5f43a62928c1c2657e337d960fe52c85f0b0
kcrombie <kelly.crombie@caci.com>
2018-07-12 09:14:17 -0600

    Builder-Documentation

--- open-sphere-base/mantle/src/main/java/io/opensphere/mantle/iconproject/impl/IconBuilderProjPane.java
+++ open-sphere-base/mantle/src/main/java/io/opensphere/mantle/iconproject/impl/IconBuilderProjPane.java
@@ -34,8 +34,8 @@ public class IconBuilderProjPane extends BorderPane
     /** The rotation value model. */
     private final DoubleProperty myRotation = new SimpleDoubleProperty(0.);
 
-    /** The size value model. */
-    private final DoubleProperty mySize = new SimpleDoubleProperty(1.);
+    /** The scale value model. */
+    private final DoubleProperty myScale = new SimpleDoubleProperty(1.);
 
     /** The X value model. */
     private final DoubleProperty myXPos = new SimpleDoubleProperty(0.);
@@ -43,69 +43,64 @@ public class IconBuilderProjPane extends BorderPane
     /** The Y value model. */
     private final DoubleProperty myYPos = new SimpleDoubleProperty(0.);
 
-    /** The current icon. */
-    private IconProjProps myCurrentIcon;
-
     /** The ColorPicker that determines the icon color. */
     private ColorPicker myColorPicker;
 
     /** The ImageView that renders the modified icon. */
-    private ImageView myImageRenderView;
-
-    /** The AWT Window that owns this pane. */
-    private final Window myOwner;
+    private ImageView myIconView;
 
     /** The spinner width. */
     private final double spinwidth = 59.0;
 
-    private final IconRecord myRecord;
+    /** The IconRecord of the displayed Icon. */
+    private final IconRecord myIconRecord;
 
-    private HBox myHbox;
+    /** The purple HBox containing the displayed icon. */
+    private HBox myIconDisplay;
 
-    private Color theColor;
+    /** The Color chosen for customized icon. */
+    private Color myColor;
 
     /**
      * Constructs a new IconBuilderPane.
      *
      * @param owner the AWT Window
-     * @return
+     * @return the customized icon in the form of a writable image named
+     *         "iconOut".
      */
     public IconBuilderProjPane(Window owner, IconRecord record)
     {
-        myOwner = owner;
-        myRecord = record;
+        myIconRecord = record;
         setTop(createTop());
 
-        setCenter(myHbox = createImageView());
+        setCenter(myIconDisplay = createImageView());
         setRight(createRight());
 
         VBox bottom = createBottom();
-        BorderPane.setMargin(bottom, new Insets(10., 0., 0., 0.));
+        BorderPane.setMargin(bottom, new Insets(5., 0., 0., 0.));
         setBottom(bottom);
 
-
-
-
-        myColorPicker.setOnAction((event) -> {
-            theColor = myColorPicker.getValue();
-            updateImageColor(theColor);
+        myColorPicker.setOnAction((event) ->
+        {
+            myColor = myColorPicker.getValue();
+            updateImageColor();
         });
 
     }
 
     /**
-     * Creates the top box, which contains the icon selection & color controls.
+     * Creates the top box, which contains the color and scale controls.
      *
      * @return the icon selection & color controls
      */
     private AnchorPane createTop()
     {
         AnchorPane TopBar = new AnchorPane();
-        Spinner<Number> sizeSpinner = new Spinner<>(0.0, 3.0, 1, .1);
-        sizeSpinner.setPrefWidth(55.);
-        sizeSpinner.getValueFactory().valueProperty().bindBidirectional(mySize);
-        sizeSpinner.setEditable(true);
-        Label sizeLabel = new Label("Scale: ", sizeSpinner);
+        Spinner<Number> sizeSpin = new Spinner<>(0.0, 3.0, 1, .1);
+        sizeSpin.setPrefWidth(55.);
+        sizeSpin.getValueFactory().valueProperty().bindBidirectional(myScale);
+        sizeSpin.setEditable(true);
+        Label sizeLabel = new Label("Scale: ", sizeSpin);
         sizeLabel.setContentDisplay(ContentDisplay.RIGHT);
         AnchorPane.setRightAnchor(sizeLabel, 0.);
 
@@ -116,9 +111,10 @@ public class IconBuilderProjPane extends BorderPane
             System.out.println("Accesible role prop:   " + myColorPicker.accessibleRoleProperty());
             System.out.println("Get children unmodifiable:  " + myColorPicker.getChildrenUnmodifiable());
         });
-        myColorPicker.setOnMouseExited(event -> System.out.println("Accesible role prop time2:   " + myColorPicker.accessibleRoleProperty()));
+        myColorPicker.setOnMouseExited(
+                event -> System.out.println("Accesible role prop time2:   " + myColorPicker.accessibleRoleProperty()));
 
-        TopBar.getChildren().addAll(myColorPicker, sizeLabel, sizeSpinner);
+        TopBar.getChildren().addAll(myColorPicker, sizeLabel, sizeSpin);
         return TopBar;
     }
 
@@ -129,8 +125,8 @@ public class IconBuilderProjPane extends BorderPane
      */
     private VBox createRight()
     {
-        VBox box = new VBox(8);
-        box.setAlignment(Pos.TOP_CENTER);
+        VBox rotBox = new VBox(8);
+        rotBox.setAlignment(Pos.TOP_CENTER);
 
         Slider rotSlider = new Slider(-180, 180, 0);
         rotSlider.setOrientation(Orientation.VERTICAL);
@@ -139,20 +135,20 @@ public class IconBuilderProjPane extends BorderPane
         rotSlider.setMajorTickUnit(45);
         rotSlider.valueProperty().bindBidirectional(myRotation);
 
-        Spinner<Number> rotSpinner = new Spinner<>(-180., 180., 0.);
-        rotSpinner.setPrefWidth(40.);
-        rotSpinner.getValueFactory().valueProperty().bindBidirectional(myRotation);
-        rotSpinner.setEditable(true);
-        rotSpinner.getStyleClass().clear();
+        Spinner<Number> rotSpin = new Spinner<>(-180., 180., 0.);
+        rotSpin.setPrefWidth(40.);
+        rotSpin.getValueFactory().valueProperty().bindBidirectional(myRotation);
+        rotSpin.setEditable(true);
+        rotSpin.getStyleClass().clear();
 
-        Label rotLabel = new Label("Rotation: ", rotSpinner);
+        Label rotLabel = new Label("Rotation: ", rotSpin);
         rotLabel.setContentDisplay(ContentDisplay.BOTTOM);
 
         VBox.setVgrow(rotSlider, Priority.ALWAYS);
-        VBox.setVgrow(rotSpinner, Priority.NEVER);
-        box.getChildren().addAll(rotSlider, rotLabel, rotSpinner);
+        VBox.setVgrow(rotSpin, Priority.NEVER);
+        rotBox.getChildren().addAll(rotSlider, rotLabel, rotSpin);
 
-        return box;
+        return rotBox;
     }
 
     /**
@@ -162,118 +158,109 @@ public class IconBuilderProjPane extends BorderPane
      */
     private VBox createBottom()
     {
-        VBox box = new VBox();
-        box.setAlignment(Pos.TOP_LEFT);
+        VBox cnrlBox = new VBox();
+        cnrlBox.setAlignment(Pos.TOP_LEFT);
 
         HBox controlBox = new HBox(10);
         controlBox.setAlignment(Pos.BASELINE_LEFT);
 
-        Spinner<Number> xSpinner = new Spinner<>(-100., 100., 0., 5.);
-        xSpinner.setPrefWidth(spinwidth);
-        xSpinner.getValueFactory().valueProperty().bindBidirectional(myXPos);
-        xSpinner.setEditable(true);
+        Spinner<Number> xSpin = new Spinner<>(-100., 100., 0., 5.);
+        xSpin.setPrefWidth(spinwidth);
+        xSpin.getValueFactory().valueProperty().bindBidirectional(myXPos);
+        xSpin.setEditable(true);
 
-        Spinner<Number> ySpinner = new Spinner<>(-125., 125., 0., 5.);
-        ySpinner.setPrefWidth(spinwidth);
-        ySpinner.getValueFactory().valueProperty().bindBidirectional(myYPos);
-        ySpinner.setEditable(true);
+        Spinner<Number> ySpin = new Spinner<>(-125., 125., 0., 5.);
+        ySpin.setPrefWidth(spinwidth);
+        ySpin.getValueFactory().valueProperty().bindBidirectional(myYPos);
+        ySpin.setEditable(true);
 
-        Label xLabel = new Label("Position:  X: ", xSpinner);
+        Label xLabel = new Label("Position:  X: ", xSpin);
         xLabel.setContentDisplay(ContentDisplay.RIGHT);
 
-        Label yLabel = new Label("Y: ", ySpinner);
+        Label yLabel = new Label("Y: ", ySpin);
         yLabel.setContentDisplay(ContentDisplay.RIGHT);
 
-        controlBox.getChildren().addAll(xLabel, xSpinner, yLabel, ySpinner);
+        controlBox.getChildren().addAll(xLabel, xSpin, yLabel, ySpin);
 
         Label helpInfo = new Label("Saved icons will appear under the 'User Added' menu.");
         helpInfo.setFont(Font.font(helpInfo.getFont().getFamily(), FontPosture.ITALIC, 11));
 
-        box.getChildren().addAll(controlBox, helpInfo);
-        return box;
+        cnrlBox.getChildren().addAll(controlBox, helpInfo);
+        return cnrlBox;
     }
 
     /**
      * Creates the ImageView which renders the selected icon.
      *
-     * @return the image view
+     * @return the iconDisplayer - the Hbox containing the icon.
      */
     private HBox createImageView()
     {
-        HBox box = new HBox();
-        box.setAlignment(Pos.CENTER);
-        box.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;" + "-fx-border-insets: 5;"
-                + "-fx-border-radius: 5;" + "-fx-border-color: purple;");
+        HBox iconDisplayer = new HBox();
+        iconDisplayer.setAlignment(Pos.CENTER);
+        iconDisplayer.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
+                + "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: purple;");
 
-        myImageRenderView = new ImageView(myRecord.getImageURL().toString());
-        myImageRenderView.rotateProperty().bind(myRotation);
-        myImageRenderView.translateXProperty().bind(myXPos);
-        myImageRenderView.translateYProperty().bind(myYPos);
-        myImageRenderView.setFitWidth(150);
-        myImageRenderView.setFitHeight(150);
-        myImageRenderView.scaleXProperty().bind(mySize);
-        myImageRenderView.scaleYProperty().bind(mySize);
+        myIconView = new ImageView(myIconRecord.getImageURL().toString());
+        myIconView.rotateProperty().bind(myRotation);
+        myIconView.translateXProperty().bind(myXPos);
+        myIconView.translateYProperty().bind(myYPos);
+        myIconView.setFitWidth(150);
+        myIconView.setFitHeight(150);
+        myIconView.scaleXProperty().bind(myScale);
+        myIconView.scaleYProperty().bind(myScale);
 
-        /*ColorAdjust monochrome = new ColorAdjust();
-        monochrome.setSaturation(-1.0);
-        ColorAdjust color2 = new ColorAdjust();
-        color2.setBrightness(5.0);
-        Blend blush = new Blend(BlendMode.MULTIPLY, monochrome, color2);
+        iconDisplayer.getChildren().addAll(myIconView);
 
-        myImageRenderView.effectProperty()
-                .bind(Bindings.when(myImageRenderView.hoverProperty()).then((Effect)blush).otherwise((Effect)null));
-        */
-
-        box.getChildren().addAll(myImageRenderView);
-
-        return box;
+        return iconDisplayer;
     }
 
     /**
      * Updates the color of the selected icon when the ColorPicker selection
      * changes.
      */
-    private void updateImageColor(Color color)
+    private void updateImageColor()
     {
         Lighting lighting = new Lighting();
         lighting.setDiffuseConstant(1.0);
         lighting.setSpecularConstant(0.75);
         lighting.setSpecularExponent(0.0);
         lighting.setSurfaceScale(0.0);
-        lighting.setLight(new Light.Distant(45, 45, theColor));
+        lighting.setLight(new Light.Distant(45, 45, myColor));
 
-        if (myImageRenderView != null)
+        if (myIconView != null)
         {
-           myImageRenderView.setEffect(lighting);
+            myIconView.setEffect(lighting);
         }
     }
 
     /**
      * Retrieves the final processed image as a BufferedImage.
      *
-     * @return the image
+     * @return the the image to be saved.
      */
     public WritableImage getFinalImage()
     {
-        WritableImage result = null;
+        WritableImage iconOut = null;
 
-        if (myImageRenderView.getImage() != null)
+        if (myIconView.getImage() != null)
         {
             SnapshotParameters parameters = new SnapshotParameters();
             parameters.setFill(Color.TRANSPARENT);
-            myHbox.setStyle(null);
-            result = myHbox.snapshot(parameters, null);
+            myIconDisplay.setStyle(null);
+            iconOut = myIconDisplay.snapshot(parameters, null);
         }
-        return result;
+        return iconOut;
     }
 
     /**
      * Generates and retrieves a name for the image, if it is not null.
      *
-     * @return the image name
+     * @return the image name.
      */
     public String getImageName()
     {
-        return myRecord != null ? myRecord.getName() + "_" + myImageRenderView.getRotate() : null;
+        return myIconRecord != null ? myIconRecord.getName() + "_" + myIconView.getRotate() + "_" + myColor.getRed() + "-"
+                + +myColor.getGreen() + "-" + myColor.getBlue() : null;
     }
 }
