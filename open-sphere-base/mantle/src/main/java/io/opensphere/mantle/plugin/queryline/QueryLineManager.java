ef9cc8007b84383d2990e0d40dec4740dd66d055
Robert Kausch <kauschr@opensphere.io>
2018-08-10 11:16:14 -0600

    VORTEX-5897: Alignment of look and feel: Query Area Selection Button

--- open-sphere-base/mantle/src/main/java/io/opensphere/mantle/plugin/queryline/QueryLineManager.java
+++ open-sphere-base/mantle/src/main/java/io/opensphere/mantle/plugin/queryline/QueryLineManager.java
@@ -3,9 +3,9 @@ package io.opensphere.mantle.plugin.queryline;
 import io.opensphere.mantle.plugin.selection.LineSelectionCommandProcessor;
 
 /**
- * 
+ * Marker interface for tagging managers involved in query line operations.
  */
 public interface QueryLineManager extends LineSelectionCommandProcessor
 {
-
+    /* intentionally blank */
 }
