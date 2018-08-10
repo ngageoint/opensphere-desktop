e4aaa03f06f2faf59ae6d96f7c617c31a899d532
Robert Kausch <kauschr@opensphere.io>
2018-08-10 08:45:29 -0600

    VORTEX-5897: Alignment of look and feel: Query Area Selection Button

---
+++ open-sphere-base/mantle/src/main/java/io/opensphere/mantle/plugin/queryline/QueryLineManager.java
@@ -0,0 +1,11 @@
+package io.opensphere.mantle.plugin.queryline;
+
+import io.opensphere.mantle.plugin.selection.LineSelectionCommandProcessor;
+
+/**
+ * 
+ */
+public interface QueryLineManager extends LineSelectionCommandProcessor
+{
+
+}
---
+++ open-sphere-base/mantle/src/main/java/io/opensphere/mantle/plugin/queryline/impl/QueryLineManagerImpl.java
@@ -0,0 +1,46 @@
+package io.opensphere.mantle.plugin.queryline.impl;
+
+import java.util.Collection;
+
+import io.opensphere.core.Toolbox;
+import io.opensphere.core.geometry.PolylineGeometry;
+import io.opensphere.core.util.swing.EventQueueUtilities;
+import io.opensphere.mantle.plugin.queryline.QueryLineManager;
+import io.opensphere.mantle.plugin.selection.BufferRegionCreator;
+import io.opensphere.mantle.plugin.selection.SelectionCommand;
+
+/**
+ * 
+ */
+public class QueryLineManagerImpl implements QueryLineManager
+{
+    /** The buffer region creator **/
+    private final BufferRegionCreator myBufferRegionCreator;
+
+    /**
+     * 
+     */
+    public QueryLineManagerImpl(Toolbox toolbox)
+    {
+        myBufferRegionCreator = new BufferRegionCreator(toolbox);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see io.opensphere.mantle.plugin.selection.LineSelectionCommandProcessor#selectionOccurred(java.util.Collection,
+     *      io.opensphere.mantle.plugin.selection.SelectionCommand)
+     */
+    @Override
+    public void selectionOccurred(Collection<? extends PolylineGeometry> lines, SelectionCommand cmd)
+    {
+        switch (cmd)
+        {
+            case CREATE_BUFFER_REGION:
+                lines.forEach(l -> EventQueueUtilities.runOnEDT(() -> myBufferRegionCreator.createBuffer(l)));
+                break;
+            default:
+                break;
+        }
+    }
+}
