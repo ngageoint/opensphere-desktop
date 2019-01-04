9a20448ede0cf6bae094281c5269ff34beea7d8d
Robert Kausch <kauschr@opensphere.io>
2019-01-04 08:22:49 -0700

    Fixing merge comments.

--- open-sphere-base/core/src/main/java/io/opensphere/core/util/collections/observable/ObservableSetBase.java
+++ open-sphere-base/core/src/main/java/io/opensphere/core/util/collections/observable/ObservableSetBase.java
@@ -7,47 +7,83 @@ import javafx.collections.ObservableSet;
 import javafx.collections.SetChangeListener;
 
 /**
- * @author Andres Almiray
- * @since 2.9.0
+ * A base class for an observable set. An observable set adheres to all of the
+ * rules defined in {@link java.util.Set}, and also allows observers to track
+ * changes when they occur.
+ *
+ * @param <E> The element types stored within the set.
  */
 public abstract class ObservableSetBase<E> extends AbstractSet<E> implements ObservableSet<E>
 {
-    private SetListenerHelper<E> listenerHelper;
+    /**
+     * The helper used to manage listeners that need to be notified of set
+     * changes.
+     */
+    private SetListenerHelper<E> myListenerHelper;
 
+    /**
+     * {@inheritDoc}
+     *
+     * @see javafx.beans.Observable#addListener(javafx.beans.InvalidationListener)
+     */
     @Override
     public final void addListener(InvalidationListener listener)
     {
-        listenerHelper = SetListenerHelper.addListener(listenerHelper, listener);
-    }
-
-    @Override
-    public final void removeListener(InvalidationListener listener)
-    {
-        listenerHelper = SetListenerHelper.removeListener(listenerHelper, listener);
-    }
-
-    @Override
-    public final void addListener(SetChangeListener<? super E> listener)
-    {
-        listenerHelper = SetListenerHelper.addListener(listenerHelper, listener);
-    }
-
-    @Override
-    public final void removeListener(SetChangeListener<? super E> listener)
-    {
-        listenerHelper = SetListenerHelper.removeListener(listenerHelper, listener);
-    }
-
-    protected final void fireChange(SetChangeListener.Change<? extends E> change)
-    {
-        SetListenerHelper.fireValueChangedEvent(listenerHelper, change);
+        myListenerHelper = SetListenerHelper.addListener(myListenerHelper, listener);
     }
 
     /**
-     * Returns true if there are some listeners registered for this list.
+     * {@inheritDoc}
+     *
+     * @see javafx.beans.Observable#removeListener(javafx.beans.InvalidationListener)
+     */
+    @Override
+    public final void removeListener(InvalidationListener listener)
+    {
+        myListenerHelper = SetListenerHelper.removeListener(myListenerHelper, listener);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see javafx.collections.ObservableSet#addListener(javafx.collections.SetChangeListener)
+     */
+    @Override
+    public final void addListener(SetChangeListener<? super E> listener)
+    {
+        myListenerHelper = SetListenerHelper.addListener(myListenerHelper, listener);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see javafx.collections.ObservableSet#removeListener(javafx.collections.SetChangeListener)
+     */
+    @Override
+    public final void removeListener(SetChangeListener<? super E> listener)
+    {
+        myListenerHelper = SetListenerHelper.removeListener(myListenerHelper, listener);
+    }
+
+    /**
+     * Propagates a change out to all registered listeners.
+     *
+     * @param change the change to send to registered listeners.
+     */
+    protected final void fireChange(SetChangeListener.Change<? extends E> change)
+    {
+        SetListenerHelper.fireValueChangedEvent(myListenerHelper, change);
+    }
+
+    /**
+     * Returns <code>true</code> if there are at least one listener registered
+     * for change notifications from this list.
+     *
+     * @return <code>true</code> if there is at least one listener registered
+     *         for change notifications from this list.
      */
     protected final boolean hasListeners()
     {
-        return SetListenerHelper.hasListeners(listenerHelper);
+        return SetListenerHelper.hasListeners(myListenerHelper);
     }
 }
