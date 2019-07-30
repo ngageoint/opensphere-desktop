b36e706a4f77092237530456d7125fd48c327394
summercode18 <kelly.crombie@caci.com>
2019-07-30 10:25:23 -0600

    Update NewKeyMapFrame.java

--- open-sphere-base/core/src/main/java/io/opensphere/core/control/keybinding/NewKeyMapFrame.java
+++ open-sphere-base/core/src/main/java/io/opensphere/core/control/keybinding/NewKeyMapFrame.java
@@ -31,7 +31,6 @@ public class NewKeyMapFrame extends AbstractInternalFrame
         final JFXPanel fxPanel = new JFXPanel();
 
         setSize(748, 610);
-        // setSize(900,700);
         setPreferredSize(getSize());
         setMinimumSize(getSize());
         setTitle(TITLE);
