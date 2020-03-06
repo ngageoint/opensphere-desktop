04f1445afbfc06418124053b3ede76cf7dadf353
Matthew Lord <matthew.lord@caci.com>
2020-03-06 14:05:11 -0700

    moved strictly similar code for shapefile export to utility file

--- open-sphere-plugins/shapefile/src/main/java/io/opensphere/shapefile/ShapeFileExporter.java
+++ open-sphere-plugins/shapefile/src/main/java/io/opensphere/shapefile/ShapeFileExporter.java
@@ -1,48 +1,31 @@
 package io.opensphere.shapefile;
 
-import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.swing.JOptionPane;
 
 import gnu.trove.map.TObjectIntMap;
 import gnu.trove.map.hash.TObjectIntHashMap;
-import io.opensphere.core.common.shapefile.shapes.MultiPointRecord;
-import io.opensphere.core.common.shapefile.shapes.PointRecord;
-import io.opensphere.core.common.shapefile.shapes.PolyLineRecord;
-import io.opensphere.core.common.shapefile.shapes.PolygonRecord;
 import io.opensphere.core.common.shapefile.shapes.ShapeRecord;
-import io.opensphere.core.common.shapefile.utils.DBFColumnInfo;
 import io.opensphere.core.common.shapefile.utils.ShapefileRecord;
 import io.opensphere.core.common.shapefile.v2.ESRIShapefile;
 import io.opensphere.core.common.shapefile.v2.ESRIShapefile.Mode;
 import io.opensphere.core.export.AbstractExporter;
-import io.opensphere.core.model.LatLonAlt;
 import io.opensphere.core.model.time.TimeSpan;
 import io.opensphere.core.preferences.ListToolPreferences;
 import io.opensphere.core.util.MimeType;
 import io.opensphere.core.util.collections.CollectionUtilities;
 import io.opensphere.core.util.collections.New;
-import io.opensphere.core.util.collections.StreamUtilities;
 import io.opensphere.core.util.swing.EventQueueUtilities;
 import io.opensphere.mantle.data.DataTypeInfo;
 import io.opensphere.mantle.data.MetaDataInfo;
 import io.opensphere.mantle.data.element.DataElement;
-import io.opensphere.mantle.data.element.MapDataElement;
-import io.opensphere.mantle.data.geom.MapGeometrySupport;
-import io.opensphere.mantle.data.geom.MapLocationGeometrySupport;
-import io.opensphere.mantle.data.geom.MapPolygonGeometrySupport;
-import io.opensphere.mantle.data.geom.MapPolylineGeometrySupport;
-import io.opensphere.mantle.data.util.DataElementLocationExtractUtil;
 import io.opensphere.mantle.util.MantleToolboxUtils;
 import io.opensphere.mantle.util.TimeSpanUtility;
 
@@ -51,9 +34,6 @@ import io.opensphere.mantle.util.TimeSpanUtility;
  */
 public class ShapeFileExporter extends AbstractExporter
 {
-    /** The Constant SHAPE_FILE_EXTENSION. */
-    private static final String SHAPE_FILE_EXTENSION = ".shp";
-
     @Override
     public boolean canExport(Class<?> target)
     {
@@ -68,7 +48,7 @@ public class ShapeFileExporter extends AbstractExporter
         MetaDataInfo metaData = dataType.getMetaDataInfo();
         List<DataElement> dataElements = MantleToolboxUtils.getDataElementLookupUtils(getToolbox())
                 .getDataElements(dataType);
-        final File esriFile = enforceSuffix(file);
+        final File esriFile = ShapeFileExportUtilities.enforceSuffix(file);
 
         ESRIShapefile shapefile = new ESRIShapefile(Mode.WRITE, esriFile.getAbsolutePath());
 
@@ -88,7 +68,8 @@ public class ShapeFileExporter extends AbstractExporter
         }
 
         TObjectIntMap<String> colNameToLengthMap = determineMaxCharsForColumn(columnNames, dataElements);
-        shapefile.setMetadataHeader(getMetadataHeader(metaData, columnNames, colNameToLengthMap));
+        shapefile.setMetadataHeader(
+                ShapeFileExportUtilities.getMetadataHeader(metaData, columnNames, colNameToLengthMap, getToolbox()));
 
         int timePrecision = getToolbox().getPreferencesRegistry().getPreferences(ListToolPreferences.class)
                 .getInt(ListToolPreferences.LIST_TOOL_TIME_PRECISION_DIGITS, 0);
@@ -136,16 +117,6 @@ public class ShapeFileExporter extends AbstractExporter
     }
 
     /**
-     * Gets the stored DataTypeInfo for the layer.
-     *
-     * @return the DataTypeInfo
-     */
-    private Collection<DataTypeInfo> getElements()
-    {
-        return CollectionUtilities.filterDowncast(getObjects(), DataTypeInfo.class);
-    }
-
-    /**
      * Creates the Shapefile record.
      *
      * @param element the data element for the record
@@ -156,7 +127,7 @@ public class ShapeFileExporter extends AbstractExporter
     private ShapefileRecord createShapefileRecord(DataElement element, List<String> columnNames,
             SimpleDateFormat dateFormatter)
     {
-        ShapeRecord shape = createShapeRecord(element);
+        ShapeRecord shape = ShapeFileExportUtilities.createShapeRecord(element);
 
         Object[] metaRow = new Object[columnNames.size()];
         for (int i = 0; i < columnNames.size(); i++)
@@ -176,32 +147,6 @@ public class ShapeFileExporter extends AbstractExporter
         }
 
         return new ShapefileRecord(shape, metaRow);
-    }
-
-    /**
-     * Get the metadata header.
-     *
-     * @param metaData the meta data for the layer
-     * @param columnNames the column names of the layer
-     * @param colIndexToLengthMap the map of column names to the data length
-     * @return the metadata header
-     */
-    private List<DBFColumnInfo> getMetadataHeader(final MetaDataInfo metaData, List<String> columnNames,
-            TObjectIntMap<String> colIndexToLengthMap)
-    {
-        List<DBFColumnInfo> metadataHeader = new LinkedList<>();
-        for (String column : columnNames)
-        {
-            char type = 'C';
-            int length = Math.min(127, colIndexToLengthMap.containsKey(column) ? colIndexToLengthMap.get(column) : 100);
-            if (metaData.hasKey(column) && metaData.isKeyNumeric(getToolbox(), column))
-            {
-                type = 'N';
-                length = 18;
-            }
-            metadataHeader.add(new DBFColumnInfo(column, type, (byte) length));
-        }
-        return metadataHeader;
     }
 
     /**
@@ -233,114 +178,12 @@ public class ShapeFileExporter extends AbstractExporter
     }
 
     /**
-     * Creates a ShapeRecord for the given data element.
+     * Gets the stored DataTypeInfo for the layer.
      *
-     * @param element the data element
-     * @return the ShapeRecord
+     * @return the DataTypeInfo
      */
-    private ShapeRecord createShapeRecord(DataElement element)
+    private Collection<DataTypeInfo> getElements()
     {
-        ShapeRecord shape = null;
-        if (element instanceof MapDataElement)
-        {
-            MapGeometrySupport geometry = ((MapDataElement) element).getMapGeometrySupport();
-            if (geometry instanceof MapLocationGeometrySupport)
-            {
-                MapLocationGeometrySupport pointGeom = (MapLocationGeometrySupport) geometry;
-                shape = new PointRecord(toPoint(pointGeom.getLocation()));
-                if (pointGeom.hasChildren())
-                {
-                    List<Point2D.Double> points = toPoints(StreamUtilities
-                            .filterDowncast(pointGeom.getChildren().stream(), MapLocationGeometrySupport.class)
-                            .map(ch -> ch.getLocation()));
-                    if (!points.isEmpty())
-                    {
-                        points.add(toPoint(pointGeom.getLocation()));
-                        shape = new MultiPointRecord(points);
-                    }
-                }
-            }
-            else if (geometry instanceof MapPolylineGeometrySupport)
-            {
-                MapPolylineGeometrySupport lineGeom = (MapPolylineGeometrySupport) geometry;
-                PolyLineRecord lineRecord = new PolyLineRecord(toPoints(lineGeom.getLocations()));
-                if (lineGeom.hasChildren())
-                {
-                    StreamUtilities.filterDowncast(lineGeom.getChildren().stream(), MapPolylineGeometrySupport.class)
-                            .forEach(child -> lineRecord.addPart(toPoints(child.getLocations())));
-                }
-                shape = lineRecord;
-            }
-            else if (geometry instanceof MapPolygonGeometrySupport)
-            {
-                MapPolygonGeometrySupport polygonGeom = (MapPolygonGeometrySupport) geometry;
-                PolygonRecord polygonRecord = new PolygonRecord(toPoints(polygonGeom.getLocations()));
-                if (polygonGeom.hasChildren())
-                {
-                    StreamUtilities.filterDowncast(polygonGeom.getChildren().stream(), MapPolygonGeometrySupport.class)
-                            .forEach(child -> polygonRecord.addPart(toPoints(child.getLocations())));
-                }
-                shape = polygonRecord;
-            }
-        }
-        // Fall back to getting the shape from the metadata
-        if (shape == null)
-        {
-            MetaDataInfo metaData = element.getDataTypeInfo().getMetaDataInfo();
-            LatLonAlt lla = DataElementLocationExtractUtil.getPosition(false, metaData.getLongitudeKey(),
-                    metaData.getLatitudeKey(), metaData.getAltitudeKey(), element);
-            double lat = lla.getLatD();
-            double lon = lla.getLonD();
-            shape = new PointRecord(lon, lat);
-        }
-        return shape;
-    }
-
-    /**
-     * Converts the locations to shape file points.
-     *
-     * @param locations the locations
-     * @return the points
-     */
-    private static List<Point2D.Double> toPoints(Collection<? extends LatLonAlt> locations)
-    {
-        return toPoints(locations.stream());
-    }
-
-    /**
-     * Converts the location stream to shape file points.
-     *
-     * @param stream the location stream
-     * @return the points
-     */
-    private static List<Point2D.Double> toPoints(Stream<? extends LatLonAlt> stream)
-    {
-        return stream.map(l -> toPoint(l)).collect(Collectors.toList());
-    }
-
-    /**
-     * Converts the location to a point.
-     *
-     * @param location the location
-     * @return the point
-     */
-    private static Point2D.Double toPoint(LatLonAlt location)
-    {
-        return new Point2D.Double(location.getLonD(), location.getLatD());
-    }
-
-    /**
-     * Enforce the shapefile suffix.
-     *
-     * @param file the file to enforce suffix on
-     * @return the file post-enforcement
-     */
-    private static File enforceSuffix(final File file)
-    {
-        if (!file.getAbsolutePath().toLowerCase().endsWith(SHAPE_FILE_EXTENSION))
-        {
-            return new File(file.getAbsolutePath() + SHAPE_FILE_EXTENSION);
-        }
-        return file;
+        return CollectionUtilities.filterDowncast(getObjects(), DataTypeInfo.class);
     }
 }
